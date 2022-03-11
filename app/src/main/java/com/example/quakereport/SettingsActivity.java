ea19d00322ca96df9a969714612f9c0661a5bea1
mdminhaziftekhar <mdminhaziftekhar@gmail.com>
2022-03-11 21:38:51 +0600

    After adding the settings menu

---
+++ app/src/main/java/com/example/quakereport/SettingsActivity.java
@@ -0,0 +1,19 @@
+package com.example.quakereport;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.preference.PreferenceFragment;
+
+public class SettingsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.settings_activity);
+    }
+
+    public static class EarthquakePreferenceFragment extends PreferenceFragment{
+
+    }
+}
